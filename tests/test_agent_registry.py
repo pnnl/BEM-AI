a126c826c39690d4f9ceea6532ef2851532f1dc4
weilixu <weili.xu@pnnl.gov>
2026-05-06 15:22:51 -0700

    address path missing / bug

--- tests/test_agent_registry.py
+++ tests/test_agent_registry.py
@@ -41,6 +41,8 @@ def test_base_url_path_parsed_from_no_scheme_url():
     assert server.host_name == "localhost"
     assert server.port == 20000
     assert server.base_url_path == "/a2a"
+    card_data = MessageToDict(server.card, preserving_proto_field_name=False)
+    assert card_data["supportedInterfaces"][0]["url"] == "http://localhost:20000/a2a/"
 
 
 def test_base_url_path_override_wins():
@@ -48,7 +50,7 @@ def test_base_url_path_override_wins():
     server = A2AAgentServer(lambda: None, card, base_url_path="/permit")
     assert server.base_url_path == "/permit"
     card_data = MessageToDict(server.card, preserving_proto_field_name=False)
-    assert card_data["supportedInterfaces"][0]["url"] == "http://localhost:20000/permit"
+    assert card_data["supportedInterfaces"][0]["url"] == "http://localhost:20000/permit/"
 
 
 def test_base_url_path_override_does_not_mutate_input_card():
